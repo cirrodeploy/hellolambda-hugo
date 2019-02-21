6268634c107378c4f7dcbc9d0bf008d8345f2d23
Chris Hamilton <chris@fyberlabs.com>
2019-02-21 09:38:12 +0800

    working hugo setup

---
+++ site/static/js/api.js
@@ -0,0 +1,42 @@
+function baseURL() {
+    return (window.useLocalhost)
+        ? `http://localhost:${HLCONFIG.FLASK_PORT}`
+        : HLCONFIG.ROOT_API_AWS
+
+}
+
+function apiURL(path) {
+    const separator = path.slice(0,1) === '/' ? '' : '/'
+    const href = baseURL() + separator + path
+    return href
+}
+
+function apiCall(path, method, data, options = {}){
+    const href = apiURL(path)
+
+    $.ajax({
+        url: href,
+        method: method,
+        data: (method.toLowerCase() === 'post') ? JSON.stringify(data): data,
+        contentType: "text/plain",
+
+        success: (data, textStatus, jqXHR) => {
+            if (options.debug) {
+                console.log("apiCall success", data, textStatus, jqXHR)
+            }
+
+            if (options.successFn) {
+                options.successFn(data, textStatus, jqXHR)
+            }
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+            if (options.debug) {
+                console.log('apiCall Error', jqXHR, textStatus, errorThrown)
+            }
+
+            if (options.errorFn) {
+                options.errorFn(jqXHR, textStatus, errorThrown)
+            }
+        }
+    })
+}
---
+++ site/static/js/config.js
@@ -0,0 +1,11 @@
+/*
+ This config mirrors many of the config parameters in hellolambda_api/config.py
+ They need to stay in sync.
+ */
+
+ const HLCONFIG = {
+  ROOT_API_AWS : 'https://0hltj8lxx8.execute-api.us-west-2.amazonaws.com/dev',
+  FLASK_PORT: 5285
+ }
+
+
